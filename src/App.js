ef9c0ad98a823bef8782a0f72834aac0774911c7
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-08 21:13:05 +0100

    ajout des donnees d'une musique

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React , {useState} from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
+import './styles/app.scss';
+import data from './util'
 
 
 function App() {
+
+  const [songs,setSongs] = useState(data())
+
   return (
     <div className="App">
       <Song />
