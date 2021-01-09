c53f158315e4ffe2dff5d8df9bed54b7aa510642
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-09 22:13:50 +0100

    ajout pause/play icone & drag/drop barre

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,24 @@
-import React, {useRef} from 'react'
+import React, {useRef , useState} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlay , faAngleLeft , faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import {faPlay , faAngleLeft , faAngleRight ,faPause } from '@fortawesome/free-solid-svg-icons';
 
 const Player = ({currentSong,isPlaying,seIsPlaying }) => {
 
+    // REF
     const audioRef = useRef(null);
+    // STATE
+    const [songInfo, setSongInfo] = useState({
+        currentTime:0,
+        duration:0
+    })
 
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        )
+    }
 
+    // Event Handlers
     const playSongHandler = () => {
         if(isPlaying){
             audioRef.current.pause()
@@ -18,20 +30,37 @@ const Player = ({currentSong,isPlaying,seIsPlaying }) => {
         
     }
 
+    const timeUpdateHandler = (e) => {
+        const currentTime = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo,currentTime,duration})
+    }
+
+    const dragHandler = (e) => {
+        const currentTime = e.target.value
+        audioRef.current.currentTime = currentTime
+        setSongInfo({...songInfo,currentTime})
+    }
+
     return(
      <div className="player">
         <div className="time-control">
-            <p>Start</p>
-            <input type="range"/>
-            <p>End</p>
+            <p>{getTime(songInfo.currentTime)}</p>
+            <input onChange={dragHandler} min={0} max={songInfo.duration} value={songInfo.currentTime} type="range"/>
+            <p>{getTime(songInfo.duration)}</p>
         </div>
         <div className="play-control">
             <FontAwesomeIcon size="2x" className="skip-back" icon={faAngleLeft}/>
-            <FontAwesomeIcon size="2x"  className="play" icon={faPlay}  onClick={playSongHandler}/>
+            <FontAwesomeIcon size="2x"  className="play" icon={isPlaying ? faPause : faPlay}  onClick={playSongHandler}/>
             <FontAwesomeIcon size="2x"  className="skip-forward" icon={faAngleRight}/>
 
         </div>
-        <audio ref={audioRef} src={currentSong.audio}></audio>
+        <audio 
+        onLoadedMetadata={timeUpdateHandler} 
+        onTimeUpdate={timeUpdateHandler} 
+        ref={audioRef} 
+        src={currentSong.audio}>
+        </audio>
     </div>
     )
 }
--- src/util.js
+++ src/util.js
@@ -3,22 +3,22 @@ import {v4 as uuidv4} from 'uuid';
 function chillHop() {
     return [
         {
-            name: "Beaver Creek",
-            cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-            artist: "Aso, Middle School, Aviino",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=10076",
-            id: uuidv4(),
-            active: true,
-            color:["#2D6765","#122E2A"]
-        },
-        {
             name: "Jazz Cabbage",
             cover: "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
             artist: "Ian Ewing, Strehlow",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
             id: uuidv4(),
-            active: false,
+            active: true,
             color:["#C15248","#F7B595"]
+        },
+        {
+            name: "Beaver Creek",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            artist: "Aso, Middle School, Aviino",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10076",
+            id: uuidv4(),
+            active: false,
+            color:["#2D6765","#122E2A"]
         },
         {
             name: "Daylight",
