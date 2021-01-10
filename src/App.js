e9b32fcd153173b45d317b67a94e49f5238391b6
jordan62300 <jordanfievetpro@outlook.fr>
2021-01-10 23:20:29 +0100

    added toggle library button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Song from "./components/Song";
 import Player from "./components/Player";
 import Library from "./components/Library"
 import './styles/app.scss';
-import data from './util'
+import data from './util';
+import Nav from './components/Nav'
 
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
   const [songs,setSongs] = useState(data())
   const [currentSong,setCurrentSong] = useState(songs[0])
   const [isPlaying, seIsPlaying] = useState(false);
+  const [libraryStatus,setLibraryStatus] = useState(false);
+
+  // handler
 
   const timeUpdateHandler = (e) => {
     const currentTime = e.target.currentTime;
@@ -28,9 +32,10 @@ function App() {
 
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong}/>
       <Player setSongInfo={setSongInfo} songInfo={songInfo} audioRef={audioRef} isPlaying={isPlaying}  seIsPlaying={seIsPlaying} currentSong={currentSong}/>
-      <Library setSongs={setSongs} audioRef={audioRef}  setCurrentSong={setCurrentSong} songs={songs} isPlaying={isPlaying}/>
+      <Library libraryStatus={libraryStatus} setSongs={setSongs} audioRef={audioRef}  setCurrentSong={setCurrentSong} songs={songs} isPlaying={isPlaying}/>
       <audio 
         onLoadedMetadata={timeUpdateHandler} 
         onTimeUpdate={timeUpdateHandler} 
